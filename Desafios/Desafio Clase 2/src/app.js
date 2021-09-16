a27ccede748cb4167eb78adb8629647d05e1718f
Nacho Koch <nacho_francos@hotmail.com>
2021-09-15 22:07:06 -0300

    Entrega segundo desafio.

---
+++ Desafios/Desafio Clase 2/src/app.js
@@ -0,0 +1,12 @@
+let nombre = prompt(`Cual es tu nombre?`)
+
+let numero = prompt (`Hola ${nombre}! Decime un numero entre 1 y 300`)
+
+if(numero >= 1 && numero <= 1000){
+    alert(`Muy bien ${nombre}, podes seguir instrucciones basicas!`)
+}else{
+    alert(`Uhhhh ${nombre}, estas fuera de rango! Proba nuevamente.`)
+};
+
+
+
