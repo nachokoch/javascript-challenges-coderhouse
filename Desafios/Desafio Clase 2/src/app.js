3a01b7118871a54a4fc0915a3b311d69e6f5f6cb
Nacho Koch <nacho_francos@hotmail.com>
2021-09-15 22:18:32 -0300

    Se corrige el maximo y se agrega parseInt

--- Desafios/Desafio Clase 2/src/app.js
+++ Desafios/Desafio Clase 2/src/app.js
@@ -1,8 +1,9 @@
 let nombre = prompt(`Cual es tu nombre?`)
 
 let numero = prompt (`Hola ${nombre}! Decime un numero entre 1 y 300`)
+let numeroParseado = parseInt(numero)
 
-if(numero >= 1 && numero <= 1000){
+if(numeroParseado >= 1 && numeroParseado <= 300){
     alert(`Muy bien ${nombre}, podes seguir instrucciones basicas!`)
 }else{
     alert(`Uhhhh ${nombre}, estas fuera de rango! Proba nuevamente.`)
